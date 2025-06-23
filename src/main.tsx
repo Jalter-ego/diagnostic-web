46b326ee5133f84c9384245d81300ed07c46c723
Jalter-ego <jhademels123@gmail.com>
2025-06-23 09:42:32 -0400

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,6 @@ import { ClerkProvider } from '@clerk/clerk-react'
 import { OrganizationProvider } from './hooks/organizationContex.tsx'
 import './index.css'
 
-
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 if (!PUBLISHABLE_KEY) {
   throw new Error('Missing Publishable Key')
