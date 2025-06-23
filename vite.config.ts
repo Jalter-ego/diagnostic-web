be781e895cc470281b2540dcdcf07fbce50ac8e5
Jalter-ego <jhademels123@gmail.com>
2025-06-23 10:29:14 -0400

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,38 +1,43 @@
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react-swc";
 import { VitePWA } from "vite-plugin-pwa";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import path from "path";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-    VitePWA({
-      registerType: "autoUpdate",
-      includeAssets: [
-        "favicon.ico",
-        "apple-touch-icon.png",
-        "android-chrome-192x192.png",
-        "android-chrome-512x512.png",
-        "favicon-16x16.png",
-        "favicon-32x32.png",
-      ],
-      workbox:{
-        maximumFileSizeToCacheInBytes: 3000000,
-      }
-    }),
-  ],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    plugins: [
+      react(),
+      tailwindcss(),
+      VitePWA({
+        registerType: "autoUpdate",
+        includeAssets: [
+          "favicon.ico",
+          "apple-touch-icon.png",
+          "android-chrome-192x192.png",
+          "android-chrome-512x512.png",
+          "favicon-16x16.png",
+          "favicon-32x32.png",
+        ],
+        workbox: {
+          maximumFileSizeToCacheInBytes: 3000000,
+        },
+      }),
+    ],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
-  build: {
-    outDir: 'dist'
-  },
-  define: {
-    'import.meta.env.VITE_CLERK_PUBLISHABLE_KEY': JSON.stringify(process.env.VITE_CLERK_PUBLISHABLE_KEY),
-  },
+    build: {
+      outDir: "dist",
+    },
+    define: {
+      "import.meta.env.VITE_CLERK_PUBLISHABLE_KEY": JSON.stringify(
+        env.VITE_CLERK_PUBLISHABLE_KEY
+      ),
+    },
+  };
 });
