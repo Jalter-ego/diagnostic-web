17e17e64c21db0f2647c7cfbd462d96e182851cd
Jalter-ego <jhademels123@gmail.com>
2025-06-23 20:56:15 -0400

    feat: change api to deploy

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,3 +1,3 @@
 //export const apilocal = 'http://localhost:3000'
-export const apilocal = 'https://nest-project-image-recognition.onrender.com'
+export const apilocal = 'https://sw-api-bcd6dfefd5gudbba.mexicocentral-01.azurewebsites.net'
 export const apiModel = 'https://fastapi-skin-model.onrender.com'
