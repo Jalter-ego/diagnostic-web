e550d99047c05b57a181c83494c295ea3cf47b56
Jalter-ego <jhademels123@gmail.com>
2025-06-25 13:35:14 -0400

    feat: add changes

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,3 +1,4 @@
 //export const apilocal = 'http://localhost:3000'
-export const apilocal = 'https://sw-gxabgjhcbpewhufb.centralus-01.azurewebsites.net'
+//export const apilocal = 'https://sw-gxabgjhcbpewhufb.centralus-01.azurewebsites.net'
+export const apilocal = 'https://nest-project-image-recognition-fmlv.onrender.com'
 export const apiModel = 'https://fastapi-skin-model.onrender.com'
