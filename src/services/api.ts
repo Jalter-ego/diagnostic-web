115ea843ab5d47a67f5c423c7cf37e701116046a
jalter-ego <jhademels123@gmail.com>
2025-09-20 22:39:00 -0400

    feat: update api of backend

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,4 +1,4 @@
 //export const apilocal = 'http://localhost:3000'
-//export const apilocal = 'https://sw-gxabgjhcbpewhufb.centralus-01.azurewebsites.net'
-export const apilocal = 'https://nest-project-image-recognition-fmlv.onrender.com'
+export const apilocal = 'backend-diagnostic-f4c0f5a0cdhtg5fj.canadacentral-01.azurewebsites.net'
+//export const apilocal = 'https://nest-project-image-recognition-fmlv.onrender.com'
 export const apiModel = 'https://fastapi-skin-model.onrender.com'
